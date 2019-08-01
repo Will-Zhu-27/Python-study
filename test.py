66486be531e987f428094a40a7eb50bf5061e836
Will_Zhu <38975620+Will-Zhu-27@users.noreply.github.com>
2019-08-01 17:46:28 +1000

    8.1

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-first_name = "yuqiang"
-last_name = "zhu"
-full_name = first_name + " " + last_name
-print("title format: " + full_name.title() + "\n")
-print("upper format: " + full_name.upper() + "\n")
-print("lower format: " + full_name.lower() + "\n")
+test_list = ["name", 24, 12.3]
+print(test_list)
+test_tuple = ("name", 23, 31.5)
+print(test_tuple)
