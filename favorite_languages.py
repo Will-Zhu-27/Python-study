1bec2de7640215a0ed8501a77e934a739af9a3c4
Will_Zhu MacBook Pro <will_zyq@qq.com>
2019-08-07 01:02:36 +1000

    8.7

--- favorite_languages.py
+++ favorite_languages.py
@@ -5,4 +5,32 @@ favorite_languages = {
     'phil':'python',
 }
 
-print("Saran's favorite language is " + favorite_languages['sarah'].title() + ".")
+'''
+for name in favorite_languages:
+    print(name.title())
+'''
+
+'''
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        print("  Hi  " + name.title() + 
+            ", I see your favorite language is " +
+            favorite_languages[name].title() + "!")
+'''
+'''
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+'''
+
+'''
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+'''
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
